58550776dd12c78434619bca46ebe64e02c812ff
Moustapha-Toure <ToureMMoustapha@gmail.com>
2023-02-21 01:16:32 -0600

    POW-17 changes along with new login or sign page font switch

---
+++ powershell/components/Footer.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import Link from 'next/link'
+import { FaRegCopyright } from 'react-icons/Fa'
+
+
+const Footer = () => {
+    return (
+        <div className='footerContainer noselect' id='powershellFooter'>
+
+            <div className='flexedFooter'>
+                <div className='siteMapContainer' id='siteMap'>
+                    <h1 className='siteDescription' id='siteMapText'>
+                        SITE MAP
+                    </h1>
+                    <div className='siteItemContainer'>
+                        <div className='siteItems' id='toHomePage'>
+                            <Link href='./Home'>
+                                <p className='siteMapLabel'>Home Page</p>
+                            </Link>
+                        </div>
+                        <div className='siteItems' id='toUpdateProjects'>
+                            <Link href='./'>
+                                <p className='siteMapLabel'>Update a Project</p>
+                            </Link>
+                        </div>
+                        <div className='siteItems' id='toCreateProjects'>
+                            <Link href='./'>
+                                <p className='siteMapLabel'>Create New Project</p>
+                            </Link>
+                        </div>
+                        <div className='siteItems' id='toProjectDetails'>
+                            <Link href='./'>
+                                <p className='siteMapLabel'>Project Details</p>
+                            </Link>
+                        </div>
+                        <div className='siteItems' id='toUserAccount'>
+                            <Link href='./'>
+                                <p className='siteMapLabel'>View Account</p>
+                            </Link>
+                        </div>
+                        <div className='siteItems' id='toHardwareOverview'>
+                            <Link href='./'>
+                                <p className='siteMapLabel'>Hardware Count</p>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='teamContainer' id='theTeam'>
+                    <h1 className='siteDescription' id='teamDescriptionText'>SHELL-TEAM</h1>
+                    <div className='shellTeamMembers'>
+                        <p className='shellMembers'>Ethan Blumenfeld</p>
+                        <p className='shellMembers'>Paul Clauss</p>
+                        <p className='shellMembers'>Rifah Tasnim</p>
+                        <p className='shellMembers'>Moustapha Toure</p>
+                        <p className='shellMembers'>Justin Ye</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className='copyrightContainer'>
+                <p className='copyrightText'>ECE 461L PowerShell</p>
+                <FaRegCopyright className='copyrightText' id='copyrightIcon' style={{ color: 'white' }} />
+                <p className='copyrightText' id='crSpring'>Spring 2023</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
---
+++ powershell/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Link from 'next/link'
+import { TbBox } from 'react-icons/tb'
+import { MdOutlineAccountCircle } from 'react-icons/Md'
+
+const Navbar = () => {
+    return (
+        <div className='navbarContainer'>
+            <div className='pshellNavLogo' id='pshellLogoIcon'>
+                <Link href='/Home'>
+                    <div className='navLink' id='homeLink'>
+                        <h1 id='webname'>PowerShell</h1>
+                    </div>
+                </Link>
+            </div>
+            <div className='regNavLinks'>
+                <div className='hardwareLogo' id='hardwareIcon'>
+                    <Link href='./'>
+                        <div className='navLink' id='hardwareLink'>
+                            <div className='iconLogo' id='boxIcon'>
+                                <TbBox />
+                            </div>
+                            <h6 className='navbarDesc'>Hardware</h6>
+                        </div>
+                    </Link>
+                </div>
+                <div className='accountLogo' id='accountIcon'>
+                    <Link href='./'>
+                        <div className='navLink' id='accountLink'>
+                            <div className='iconLogo' id='profileIcon'>
+                                <MdOutlineAccountCircle />
+                            </div>
+                            <h6 className='navbarDesc'>Account</h6>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
